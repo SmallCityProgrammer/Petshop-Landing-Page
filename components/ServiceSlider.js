b5f5125751043e5a89ce9f0da738755a15166dc6
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-30 11:32:32 -0300

    Services

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import Image from 'next/image';
+
+import {Swiper, SwiperSlide } from 'swiper/react';
+
+import { Navigation } from 'swiper';
+
+import 'swiper/css/navigation';
+import 'swiper/css';
+
 const ServiceSlider = () => {
   return <div>ServiceSlider</div>;
 };
